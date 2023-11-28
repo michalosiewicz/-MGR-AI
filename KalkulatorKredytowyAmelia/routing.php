4d7912a2b8d3670cb5f6b7aec88941d29f6607b8
michalosiewicz <43855081+michalosiewicz@users.noreply.github.com>
2023-11-28 22:12:06 +0100

    Delete KalkulatorKredytowyAmelia directory

--- KalkulatorKredytowyAmelia/routing.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use core\App;
-use core\Utils;
-
-App::getRouter()->setDefaultRoute('init'); #default action
-
-Utils::addRoute('init','KalkulatorKredytowy');
-Utils::addRoute('oblicz','KalkulatorKredytowy');
